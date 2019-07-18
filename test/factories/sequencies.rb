052036f2358e10ebad1996208ece63961303e8a2
Vasliy Varakin <vasliyvarakin@Vasliys-MacBook-Pro.local>
2019-07-18 14:40:14 +0400

    refact sequence

--- test/factories/sequencies.rb
+++ test/factories/sequencies.rb
@@ -4,16 +4,10 @@ FactoryBot.define do
   sequence :email do |n|
     "person#{n}@example.com"
   end
-  sequence :string, aliases: %i[name surname] do |n|
+  sequence :string, aliases: %i[name surname short_description full_description] do |n|
     "user-#{n}"
   end
   sequence :integer, aliases: %i[price_cents amount] do |n|
     rand(1...100)
-  end
-  sequence :short_description do
-    "delicious product"
-  end
-  sequence :full_description do
-    "very tasty product for the price of one piece"
   end
 end
