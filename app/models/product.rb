0b2cebdca76a04cb8eef7c544485251b232de61a
Anna Rozhdestvenskaya <anya.rozhdestvenskaya@gmail.com>
2019-07-31 13:03:44 +0400

    Add price's error transfer

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,4 +8,15 @@ class Product < ApplicationRecord
   validates :full_description, length: { maximum: 500 }
 
   monetize :price_cents
+
+  after_validation :transfer_price_errors
+
+  private
+    def transfer_price_errors
+      messages = errors[:price_cents]
+      messages.each do |message|
+        errors.add(:price, message)
+        errors.delete(:price_cents)
+      end
+    end
 end
